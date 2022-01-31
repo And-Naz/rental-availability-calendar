f6e184dc19b2c2e6a25f6372aef6a00cd2968c0f
And-Naz <nazaryan-ando@mail.ru>
2022-01-31 18:24:45 +0400

    Normalize requests to AcuRNT api

--- src/api/apiInterface/acumatica/AcuRNT.js
+++ src/api/apiInterface/acumatica/AcuRNT.js
@@ -6,18 +6,35 @@ const AcuRNT = {
 	},
 	loadRecords: async function (filter, start = this.initStart, end = this.initEnd) {
 		const _filter = this._getKeys(filter)
-		console.log(_filter);
-		console.log(`start ${start} end ${end}`);
-		const retVal = await axios.post(this.UrlGeneration("LoadRecords"), { filter: _filter, start, end }, this.Configs)
+		const retVal = await axios.post(
+			this.UrlGeneration("LoadRecords"),
+			{ filter: _filter, start, end },
+			this.Configs
+		).then(response => response.data.d)
 		console.log(retVal);
-		return []
+		return retVal
 	},
 	recordsTotalCount: async function (filter) {
 		const _filter = this._getKeys(filter)
-		console.log(_filter);
-		const retVal = await axios.post(this.UrlGeneration("RecordsTotalCount"), {filter: _filter}, this.Configs)
+		const retVal = await axios.post(
+			this.UrlGeneration("RecordsTotalCount"),
+			{filter: _filter},
+			this.Configs
+		).then(response => response.data.d)
 		console.log(retVal);
-		return []
+		return retVal
+	},
+	helpers: {
+		getOrderInfoOfItems: async function (filter, items) {
+			const _filter = this._getKeys(filter)
+			const retVal = await axios.post(
+				this.UrlGeneration("GetOrderInfoOfItems"),
+				{ filter: _filter, items },
+				this.Configs
+			).then(response => response.data.d)
+			console.log(retVal);
+			return retVal
+		},
 	},
 	_getKeys: function (keySource) {
 		let key
@@ -28,90 +45,6 @@ const AcuRNT = {
 			return acc
 		}, {})
 	},
-	helpers: {
-		getOrderInfoOfItems: async function (filter, items) {
-			const _filter = this._getKeys(filter)
-			console.log(_filter);
-			const retVal = await axios.post(this.UrlGeneration("GetOrderInfoOfItems"), { filter: _filter, items }, this.Configs)
-			console.log(retVal);
-			return []
-		}
-	}
-	// GetRentalOrders: async function (startDate, endDate, radioValue) {
-	// 	const params = { startDateStr: '', endDateStr: '', allowShipped: false, allowNotShipped: false, allOpened: false }
-	// 	if (startDate instanceof Date) {
-	// 		params.startDateStr = startDate.$formatDate("YYYY-MM-DD")
-	// 	} else if (typeof startDate === "string") {
-	// 		params.startDateStr = startDate
-	// 	} else {
-	// 		return []
-	// 	}
-	// 	if (endDate instanceof Date) {
-	// 		params.endDateStr = endDate.$formatDate("YYYY-MM-DD")
-	// 	} else if (typeof endDate === "string") {
-	// 		params.startDateStr = endDate
-	// 	} else {
-	// 		return []
-	// 	}
-	// 	switch (radioValue) {
-	// 		case "1":
-	// 			params.allOpened = true
-	// 			break;
-	// 		case "2":
-	// 			params.allowShipped = true
-	// 			break;
-	// 		default:
-	// 			params.allowNotShipped = true
-	// 			break;
-	// 	}
-	// 	let retVal = await axios.post(this.UrlGeneration("GetRentalOrders"), params, this.Configs)
-	// 		.then(response => response.data.d)
-	// 	return retVal
-	// },
-	// GetRentalItems: async function () {
-	// 	let retVal = await axios.post(this.UrlGeneration("GetRentalItems"), {}, this.Configs)
-	// 		.then(response => response.data.d)
-	// 	return retVal
-	// },
-	// GetRentalItemsOrders: async function (startDate, endDate, radioValue, selectedList) {
-	// 	const params = { startDateStr: '', endDateStr: '', allowShipped: false, allowNotShipped: false, allOpened: false, itemInfoList: [] }
-	// 	if (startDate instanceof Date) {
-	// 		params.startDateStr = startDate.$formatDate("YYYY-MM-DD")
-	// 	} else if (typeof startDate === "string") {
-	// 		params.startDateStr = startDate
-	// 	} else {
-	// 		return []
-	// 	}
-	// 	if (endDate instanceof Date) {
-	// 		params.endDateStr = endDate.$formatDate("YYYY-MM-DD")
-	// 	} else if (typeof endDate === "string") {
-	// 		params.startDateStr = endDate
-	// 	} else {
-	// 		return []
-	// 	}
-	// 	switch (radioValue) {
-	// 		case "1":
-	// 			params.allOpened = true
-	// 			break;
-	// 		case "2":
-	// 			params.allowShipped = true
-	// 			break;
-	// 		case "3":
-	// 			params.allowNotShipped = true
-	// 			break;
-	// 		default:
-	// 			params.allOpened = true
-	// 			break;
-	// 	}
-	// 	if (!Array.isArray(selectedList)) {
-	// 		return [];
-	// 	} else {
-	// 		params.itemInfoList = selectedList
-	// 	}
-	// 	let retVal = await axios.post(this.UrlGeneration("GetRentalItemsOrders"), params, this.Configs)
-	// 		.then(response => response.data.d)
-	// 	return retVal
-	// },
 }
 
 export default AcuRNT
