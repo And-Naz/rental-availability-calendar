d96c5f210510ec7d6f9495fac8eec210cbf0491f
And-Naz <nazaryan-ando@mail.ru>
2022-02-01 16:38:00 +0400

    Fixing issue related helpers of request when selectBy is equal ItemsType

--- src/api/apiInterface/acumatica/AcuRNT.js
+++ src/api/apiInterface/acumatica/AcuRNT.js
@@ -1,13 +1,10 @@
 import axios from 'axios'
 const AcuRNT = {
 	Configs: { headers: { "Content-Type": "application/json" } },
-	UrlGeneration: function (url) {
-		return window.location.origin + window.location.pathname + '/' + url
-	},
 	loadRecords: async function (filter, start = this.initStart, end = this.initEnd) {
-		const _filter = this._getKeys(filter)
+		const _filter = getKeys(filter)
 		const retVal = await axios.post(
-			this.UrlGeneration("LoadRecords"),
+			UrlGeneration("LoadRecords"),
 			{ filter: _filter, start, end },
 			this.Configs
 		).then(response => response.data.d)
@@ -15,9 +12,9 @@ const AcuRNT = {
 		return retVal
 	},
 	recordsTotalCount: async function (filter) {
-		const _filter = this._getKeys(filter)
+		const _filter = getKeys(filter)
 		const retVal = await axios.post(
-			this.UrlGeneration("RecordsTotalCount"),
+			UrlGeneration("RecordsTotalCount"),
 			{filter: _filter},
 			this.Configs
 		).then(response => response.data.d)
@@ -26,25 +23,30 @@ const AcuRNT = {
 	},
 	helpers: {
 		getOrderInfoOfItems: async function (filter, items) {
-			const _filter = this._getKeys(filter)
 			const retVal = await axios.post(
-				this.UrlGeneration("GetOrderInfoOfItems"),
-				{ filter: _filter, items },
+				UrlGeneration("GetOrderInfoOfItems"),
+				{ filter, items },
 				this.Configs
-			).then(response => response.data.d)
+			).then(response => response.data.d, console.error)
+			.finally(() => {console.log("getOrderInfoOfItems finished");})
 			console.log(retVal);
 			return retVal
 		},
 	},
-	_getKeys: function (keySource) {
-		let key
-		let value;
-		return keySource.split("|").reduce((acc, str) => {
-			[key, value] = str.split(":")
-			acc[key] = value
-			return acc
-		}, {})
-	},
+}
+
+function getKeys(keySource) {
+	let key
+	let value;
+	return keySource.split("|").reduce((acc, str) => {
+		[key, value] = str.split(":")
+		acc[key] = value
+		return acc
+	}, {})
+}
+
+function UrlGeneration(url) {
+	return window.location.origin + window.location.pathname + '/' + url
 }
 
 export default AcuRNT
--- src/api/index.js
+++ src/api/index.js
@@ -4,5 +4,5 @@ const api = new Request(apiInterFace)
 export default api;
 
 export function getOrderInfoOfItems(filter, items) {
-    return api.helpers['getOrderInfoOfItems'] ? api.helpers['getOrderInfoOfItems'](filter, items) : []
+    return api.helpers ? api.helpers.getOrderInfoOfItems(filter, items) : []
 }
